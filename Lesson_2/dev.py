b47bb97a37a8b0f185f97b98a07aee6b98662f02
silvaxxx1 <silvapi1994@gmail.com>
2025-04-26 23:58:14 +0300

    add to new code

---
+++ Lesson_2/dev.py
@@ -0,0 +1,34 @@
+# import all the required libraries
+
+
+# calculate the area of a circle
+
+
+# calculate the perimeter of a circle
+
+
+# calculate the area of a rectangle
+
+
+# calculate the perimeter of a rectangle
+
+
+# calculate the area of a triangle
+
+
+# calculate the perimeter of a triangle
+
+
+
+
+
+
+# define the main function
+def shape_calculator():
+    pass 
+
+
+
+
+# call the function 
+shape_calculator()
